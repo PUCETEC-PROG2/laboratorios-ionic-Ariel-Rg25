07b6fed1188e85f9c1f3829b780110654a702af2
Usuario invitado <Guest@iMac-de-PUCE-A10814.local>
2026-06-11 15:58:15 -0500

    laboratorio componentes gui de ionic

---
+++ src/components/RepoItem.tsx
@@ -0,0 +1,32 @@
+import { IonItemSliding, IonItem, IonThumbnail, IonLabel, IonItemOptions, IonItemOption, IonIcon } from '@ionic/react';
+import { pencil, trash } from 'ionicons/icons';
+import './RepoItem.css'
+import React from 'react'
+import { Repository } from '../interfaces/Repository';
+
+const RepoItem: React.FC<Repository> = (repository) => {
+    return(
+        <IonItemSliding>
+                  <IonItem>
+                    <IonThumbnail slot='start'>
+                      <img src={repository.avatarUrl} alt={repository.name} />
+                    </IonThumbnail>
+                    <IonLabel>
+                      <h3>{repository.name}</h3>
+                      <p>{repository.description}</p>
+                      <p><strong>Language: </strong>{repository.language}</p>
+                    </IonLabel>
+                  </IonItem>
+                  <IonItemOptions>
+                    <IonItemOption>
+                      <IonIcon icon={pencil} slot='icon-only'/>
+                    </IonItemOption>
+                    <IonItemOption color="danger">
+                      <IonIcon icon={trash} slot='icon-only'/>
+                    </IonItemOption>
+                  </IonItemOptions>
+                </IonItemSliding>
+    )
+}
+
+export default RepoItem
