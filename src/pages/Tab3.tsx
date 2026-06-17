e51e3ff67183061c86c16864e7b9544885216054
Usuario invitado <Guest@iMac-de-PUCE-A10814.local>
2026-06-17 15:54:26 -0500

    Formulario de creacion, Pestaña de información de usuario

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard,IonCardTitle, IonCardSubtitle, IonCardContent, IonCardHeader } from '@ionic/react';
+
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +7,27 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de Usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de Usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <div className="card-container">
+          <IonCard className="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfQcHVppvZkipCwthNivc3mSfpMvIdT-weRw2aOCAE3A&s=10" alt="Avatar" />
+            <IonCardHeader>
+              <IonCardTitle>Ariel Rueda</IonCardTitle>
+              <IonCardSubtitle>agrueda</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <p>Contenido de ejemplo para mostrar información</p>
+            </IonCardContent>
+          </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );
